f9049074d8597ddaaac646185e95ce1a0172b59c
Mike <lammara96@gmail.com>
2020-06-24 18:43:02 -0400

    fixed shit

--- src/control/ArmorFileReader.java
+++ src/control/ArmorFileReader.java
@@ -1,15 +1,11 @@
 package control;
 
-import items.Chest;
-
 public class ArmorFileReader {
 
     private final static String FILE_PATH = "src/control/styles/res/armor.xml";
 
     public static void init() {
-       // ObjectMapper objectMapper = new XmlMapper();
-        //Chest chest =
-    
+
 
     }
 
