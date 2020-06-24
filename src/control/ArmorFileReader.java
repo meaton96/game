c8b6f41ebf011982391f1dbc83236910b9e75ab2
Mike <lammara96@gmail.com>
2020-06-24 18:34:36 -0400

    added some controls to main menu and set up start button to go to character select

--- src/control/ArmorFileReader.java
+++ src/control/ArmorFileReader.java
@@ -1,11 +1,15 @@
 package control;
 
+import items.Chest;
+
 public class ArmorFileReader {
 
     private final static String FILE_PATH = "src/control/styles/res/armor.xml";
 
     public static void init() {
-
+       // ObjectMapper objectMapper = new XmlMapper();
+        //Chest chest =
+    
 
     }
 
